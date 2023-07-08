bab2c1fbd1ead589b50d0ad1b200b84c13f17020
SamuelLeutner <samuel.leutner@icloud.com>
2023-07-07 22:56:41 -0300

    change on the home file

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-   return <h1>Hello World!</h1>
+   return (
+    <main>
+        <h1>Oi mãe!</h1>
+        <h1>Só quero deixar claro que eu te amo muito, tenha um bom dia!</h1>
+        <p>estou testando uma nova forma de por um site na web</p>
+    </main>
+   )
 }
 
 export default Home
